76c2e7255db655b1fd4c2948a816a62deca15a31
Lucas Gomes <l.gomees_@hotmail.com>
2019-10-01 00:54:56 -0300

    Creating Routes base

--- cafofo/src/App.js
+++ cafofo/src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
+import Routes from './routes';
 
 import Header from './components/Header';
-import Main from "./pages/main";
 
 import './styles.css';
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Routes />
     </div>
   );
 }
--- cafofo/src/pages/main/index.js
+++ cafofo/src/pages/main/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
 export default class Main extends Component {
     state = {
-        ads: []
+        cafofoAds: []
     };
     
     //Executa a ação quando o elemento já é renderizado na tela
@@ -15,29 +16,33 @@ export default class Main extends Component {
 
     loadAds = async () => {
         const response = await api.get('/ads');
-
-        this.setState({ ads: response.data.docs })
+    
+        this.setState({ cafofoAds: response.data })
         //.data.docs serve para ler as datas dentro das requisições de Docs 
         // Mudar de acordo com o banco de dados, junto com o BASEURL em api.js
     };
 
     render() {
 
-        const { ads } = this.state;
+        const { cafofoAds } = this.state;
 
         return (
             //Adicionando lista de anúncios
-            <div className="ads-list"> 
-                {ads.map(ads => (
+            <div className="listads"> 
+                {cafofoAds.map(ads => (
                     //Para executar o .map é necessário adicionar uma key com valor único para cada elemento.
                     //Nesse caso, usamos o ID
-                    <article key={ads._id}>
+                    <article key={ads.id}>
                         <strong>{ads.title}</strong>
                         <h2>{ads.price}</h2>
-                        <h3>{ads.status}</h3>
+                        <Link to={'/cafofos/${ads._id}'}>Detalhes</Link>
                     </article>
                 ))}
             </div>
         )
+            
+        
+        
+        
     }
 }
