13bbcd763425a810a255c8b0e32c9a99690c1c8a
Victor Rodrigues <victorrsilva17@gmail.com>
2019-10-03 22:25:41 -0300

    #2 Adding Login Button on Header

--- cafofo/src/components/Header/index.js
+++ cafofo/src/components/Header/index.js
@@ -1,7 +1,20 @@
-import React from 'react';
-
+import React, {Component} from 'react';
+import Login from '../../pages/login/index'
 import './styles.css';
 
-const Header = () => <header id='main-header'>Cafofo</header>
+
+
+const Header = () =>
+    <header id='main-header'>
+        <div id='logo-container'>
+            Logo
+        </div>
+        <a id='title'>
+            Cafofo
+        </a>
+        <div id='login-container'>
+            <a id='login-button' href={<Login/>} >Entrar</a>
+        </div>
+    </header>
 
 export default Header;
---
+++ cafofo/src/pages/login/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+export default class Login extends Component {
+    render() {
+        return( 
+            <div>
+
+                <h1>
+                    Login <br />
+                    Login <br />
+                    Login <br />
+                </h1>
+            </div>
+        )
+    }
+}
