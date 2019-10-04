729c3fcd70974f6133569eae788cf1155d5c05c8
Victor Rodrigues <victorrsilva17@gmail.com>
2019-10-04 09:16:38 -0300

    #2 Login Form

--- cafofo/src/pages/login/index.js
+++ cafofo/src/pages/login/index.js
@@ -1,16 +1,64 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { Button, FormGroup, FormControl, FormLabel, Form } from "react-bootstrap";
+import "./styles.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class Login extends Component {
-    render() {
-        return( 
-            <div>
+    constructor(props) {
+        super(props);
 
-                <h1>
-                    Login <br />
-                    Login <br />
-                    Login <br />
-                </h1>
+        this.state = {
+            email: "",
+            password: ""
+        };
+    }
+
+    validateForm() {
+        return this.state.email.length > 0 && this.state.password.length > 0;
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div id="login-container">
+                <Form onSubmit={this.handleSubmit} id="login-form">
+                    <FormGroup controlId="email">
+                        <FormLabel>Email</FormLabel>
+                        <FormControl
+                            autoFocus
+                            placeholder="Digite o seu email"
+                            type="email"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <FormGroup controlId="password">
+                        <FormLabel>Senha</FormLabel>
+                        <FormControl
+                            placeholder="Senha"
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                            type="password"
+                        />
+                    </FormGroup>
+                    <Button
+                        variant="primary"
+                        disabled={!this.validateForm()}
+                        type="submit"
+                    >
+                        Login
+                    </Button>
+                </Form>
             </div>
-        )
+        );
     }
 }
