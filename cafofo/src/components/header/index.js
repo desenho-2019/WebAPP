81a0840789fbdfd653055dc3a360e44cb0e12dae
Lucas Gomes <l.gomees_@hotmail.com>
2019-10-11 11:52:06 -0300

    #4 Styling HomePage and Navbar

--- cafofo/src/components/header/index.js
+++ cafofo/src/components/header/index.js
@@ -1,21 +1,41 @@
 import React, {Component} from 'react';
-import Login from '../../pages/login/index';
+import { Nav, Navbar } from 'react-bootstrap'
 import './styles.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 
 
 const Header = () =>
-    <header id='main-header'>
-        <div id='logo-container'>
-            Logo
-        </div>
-        <a id='title'>
-            <Link to={'/'}>Cafofo</Link>
-        </a>
-        <div id='login-container'>
-            <Link to={'/login'}>Login</Link>
-        </div>
-    </header>
+<Navbar bg="light" expand="lg">
+<Navbar.Brand href="/">
+      <img
+        alt=""
+        src="/cafofo_favicon.png"
+        width="30"
+        height="25"
+        className="d-inline-block align-top"
+      />
+      {'Cafofo'}
+</Navbar.Brand>
+<Navbar.Toggle aria-controls="basic-navbar-nav" />
+<Navbar.Collapse id="basic-navbar-nav">
+  <Nav className="mr-auto">
+    <Nav.Link href="/login">Login</Nav.Link>
+    <Nav.Link href="/cadastro">Cadastro</Nav.Link>
+  </Nav>
+</Navbar.Collapse>
+</Navbar>
+
+{/* <header id='main-header'>
+    <div id='logo-container'>
+        Logo
+    </div>
+    <a id='title'>
+        <Link to={'/'}>Cafofo</Link>
+    </a>
+    <div id='login-container'>
+        <Link to={'/login'}>Login</Link>
+    </div>
+</header> */}
 
 export default Header;
--- cafofo/src/pages/main/index.js
+++ cafofo/src/pages/main/index.js
@@ -5,44 +5,13 @@ import { Link } from 'react-router-dom';
 import './styles.css';
 
 export default class Main extends Component {
-    state = {
-        cafofoAds: []
-    };
-    
-    //Executa a ação quando o elemento já é renderizado na tela
-    componentDidMount() {
-        this.loadAds();
-    }
-
-    loadAds = async () => {
-        const response = await api.get('/ads');
-    
-        this.setState({ cafofoAds: response.data })
-        //.data.docs serve para ler as datas dentro das requisições de Docs 
-        // Mudar de acordo com o banco de dados, junto com o BASEURL em api.js
-    };
 
     render() {
-
-        const { cafofoAds } = this.state;
-
         return (
-            //Adicionando lista de anúncios
-            <div className="listads"> 
-                {cafofoAds.map(ads => (
-                    //Para executar o .map é necessário adicionar uma key com valor único para cada elemento.
-                    //Nesse caso, usamos o ID
-                    <article key={ads.id}>
-                        <strong>{ads.title}</strong>
-                        <h2>{ads.price}</h2>
-                        <Link to={'/cafofos/${ads._id}'}>Detalhes</Link>
-                    </article>
-                ))}
+            <div className='mainpage'>
+              <div id='shadowimage'>
+              </div>
             </div>
-        )
-            
-        
-        
-        
+        )   
     }
 }
